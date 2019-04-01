043f3876aa30492c31d6902a4b2104fd35f9402d
Quentin BERNET <q.bernet@gmail.com>
2019-04-01 19:12:55 +0200

    Sonar vulnerability  - add GET method to RequestMapping

--- src/main/java/com/jvs/ski/rest/draft/DraftController.java
+++ src/main/java/com/jvs/ski/rest/draft/DraftController.java
@@ -4,10 +4,12 @@ package com.jvs.ski.rest.draft;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
 @RestController
 public class DraftController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(method = GET, value = "/hello")
     public String gretting() {
         return "Hello, world";
     }
