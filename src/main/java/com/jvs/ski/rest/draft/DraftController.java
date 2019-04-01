a0e3a624aec3b92e2476b0dec5dce887431721a4
Quentin BERNET <q.bernet@gmail.com>
2019-04-01 19:20:50 +0200

    Sonar debt

--- src/main/java/com/jvs/ski/rest/draft/DraftController.java
+++ src/main/java/com/jvs/ski/rest/draft/DraftController.java
@@ -1,15 +1,13 @@
 package com.jvs.ski.rest.draft;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class DraftController {
 
-    @RequestMapping(method = GET, value = "/hello")
+    @GetMapping(path = "/hello")
     public String gretting() {
         return "Hello, world";
     }
